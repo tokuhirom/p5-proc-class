0d8a34aac1b2be1a4f0fadeaafce458d7b91bc26
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-07-08 12:42:25 +0900

    initial commit

---
+++ lib/Proc/Class.pm
@@ -0,0 +1,33 @@
+package Proc::Class;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Proc::Class -
+
+=head1 SYNOPSIS
+
+  use Proc::Class;
+
+=head1 DESCRIPTION
+
+Proc::Class is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom  slkjfd gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
